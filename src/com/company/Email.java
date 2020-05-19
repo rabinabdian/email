7b9ad7c08ce682b27d490adbf03446bbcb41a2a1
ravdaian <Ruben.Avdaian@harman.com>
2020-05-19 18:14:09 +0300

    create email application adding email class

---
+++ src/com/company/Email.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Email {
+    String firstName;
+    String lastName;
+    String password;
+    String department;
+    int mailBoxCapacity;
+    String alternateEmail;
+
+    // constructor to receive first name and last name
+
+
+    //  ask for department
+
+
+    // generate random password
+
+
+
+    // set mailbox capacity
+
+
+    // set the alternate email
+
+
+    // change the password
+
+
+}
