a0f51657e8c8dbd17fd82eed88dea04a11c4141b
ravdaian <Ruben.Avdaian@harman.com>
2020-06-02 15:44:40 +0300

    create constructor for email class

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -9,6 +9,12 @@ public class Email {
     String alternateEmail;
 
     // constructor to receive first name and last name
+    public Email (String firstName,String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        System.out.println("EMAIL CREATED : " + this.firstName + " " + this.lastName);
+
+    }
 
 
     //  ask for department
